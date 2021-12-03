339d50e2d47673685002bf353a701c0c58bd2bbf
Redshore <isutare412@gmail.com>
2021-12-03 14:54:15 +0900

    Add C shared library

---
+++ rsum/include/RsMath.h
@@ -0,0 +1,8 @@
+#ifndef __RS_MATH_H__
+#define __RS_MATH_H__
+
+#include <inttypes.h>
+
+double multiply(double a, double b);
+
+#endif // __RS_MATH_H__
---
+++ rsum/src/RsMath.c
@@ -0,0 +1,6 @@
+#include "RsMath.h"
+
+double multiply(double a, double b)
+{
+    return a * b;
+}
